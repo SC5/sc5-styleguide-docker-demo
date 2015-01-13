e3453625c92ad53ccde7d3cf08ab1000054c13f1
Juuso Backman <juuso.backman@sc5.io>
2015-01-14 00:47:47 +0200

    Start server using gulp so that gulp output is logged properly

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,18 @@
 FROM sc5io/ubuntu:latest
 
 # Environment variables
-EXPOSE 3000
 ENV NODE_ENV production
-ENV SG_GIT_REPO https://github.com/SC5/sc5-styleguide-docker-demo.git
-ENV SG_GIT_BRANCH master
 ENV SG_APP_DIR /sc5-styleguide-demo
+ENV SG_PORT 3210
+EXPOSE 3210
 
 # Image build steps
+COPY scripts $SG_APP_DIR/scripts
 WORKDIR $SG_APP_DIR
-RUN npm install sc5-styleguide
-RUN npm install -g pm2
 RUN mkdir logs
+RUN npm install -g pm2
+RUN npm install
+RUN npm install sc5-styleguide@0.3.0-rc2
 
 # Image entrypoint / start command
-CMD run.sh
+CMD npm start
--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,16 @@ var path = require('path'),
   gulp = require('gulp'),
   sass = require('gulp-sass'),
   styleguide = require('sc5-styleguide'),
-  styleguideDir = path.resolve(__dirname, '../node_modules/sc5-styleguide'),
+  styleguideDir = path.resolve(__dirname, 'node_modules/sc5-styleguide'),
   source = path.join(styleguideDir, 'lib/app/**/*.scss'),
-  outputPath = path.resolve(__dirname, '../demo-output');
+  outputPath = path.resolve(__dirname, 'demo-output');
 
 gulp.task('styleguide:generate', function() {
   return gulp.src(source)
     .pipe(styleguide.generate({
       title: 'SC5 Style Guide Demo',
       server: true,
+      port: process.env.SG_PORT,
       rootPath: outputPath,
       overviewPath: path.join(styleguideDir, 'README.md')
     }))
@@ -39,5 +40,3 @@ gulp.task('styleguide:static', function() {
 gulp.task('watch', ['styleguide'], function() {
   gulp.watch(source, ['styleguide']);
 });
-
-gulp.start('watch');
