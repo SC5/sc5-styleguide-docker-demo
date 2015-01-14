a164373504e5e9d5bf33661f34f32220b278aed9
Juuso Backman <juuso.backman@sc5.io>
2015-01-14 15:04:55 +0200

    Add plumber to suppress errors. Invalid variable input from client still breaks the server.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var path = require('path'),
   gulp = require('gulp'),
   sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
   styleguide = require('sc5-styleguide'),
   rootSrc = path.resolve('.', 'demo-input'),
   source = path.join(rootSrc, 'sass/**/*.scss'),
@@ -14,6 +15,7 @@ var path = require('path'),
 gulp.task('styleguide:generate', function() {
   var pkg = require(path.resolve(__dirname, 'node_modules/sc5-styleguide/package.json'));
   return gulp.src(source)
+    .pipe(plumber())
     .pipe(styleguide.generate({
       title: pkg.name + ' ' + pkg.version + ' demo',
       server: true,
@@ -22,14 +24,17 @@ gulp.task('styleguide:generate', function() {
       overviewPath: path.join(rootSrc, 'README.md'),
       styleVariables: path.join(rootSrc, 'sass/_styleguide_variables.scss')
     }))
-    .pipe(gulp.dest(outputPath));
+    .pipe(gulp.dest(outputPath))
+    .on('error', console.error.bind(console));
 });
 
 gulp.task('styleguide:applystyles', ['styleguide:generate'], function() {
   return gulp.src(path.join(rootSrc, 'sass/app.scss'))
+    .pipe(plumber())
     .pipe(sass(sassOpts))
     .pipe(styleguide.applyStyles())
-    .pipe(gulp.dest(outputPath));
+    .pipe(gulp.dest(outputPath))
+    .on('error', console.error.bind(console));
 });
 
 gulp.task('styleguide:static', function() {
